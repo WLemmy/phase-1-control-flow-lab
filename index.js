356266a32a9456e303b5ccd20d75b957fa2407a2
Wilfred Lemayian <wilfred.lemayian@gmail.com>
2024-03-21 05:57:27 +0300

    Control Flow Lab

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distanceInFeet) {
+  if (distanceInFeet <= 400) {
+      // If the distance is less than or equal to 400 feet
+      return "This one is on me!";
+  } else if (distanceInFeet <= 2000) {
+      // If the distance is between 401 and 2000 feet
+      return "That will be twenty bucks.";
+  } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+      // If the distance is between 2001 and 2500 feet
+      return "I will gladly take your thirty bucks.";
+  } else {
+      // If the distance is over 2500 feet
+      return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+      case "generous":
+          return "Thank you so much.";
+      case "not as generous":
+          return "Thank you.";
+      default:
+          return "Bye.";
+  }
 }
